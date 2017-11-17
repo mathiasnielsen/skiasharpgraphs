c4d9cff1acc2f515303cc7861bba5fb0470726c2
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-17 15:10:29 +0000

    animating bar charts

--- Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
+++ Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
@@ -10,21 +10,23 @@ namespace SkiaSharpSimpleCharts
     public partial class SkiaSharpSimpleChartsPage : ContentPage
     {
         private SkiaSharpSimpleChartsViewModel viewModel;
+        private float animationProgress = 1.0f;
+        private float easingAnimationProgress = 1.0f;
 
-        SKPaint barPaint = new SKPaint
+        private SKPaint barPaint = new SKPaint
         {
             Style = SKPaintStyle.Fill,
             Color = SKColors.Blue
         };
 
-        SKPaint strokePaint = new SKPaint
+        private SKPaint strokePaint = new SKPaint
         {
             Style = SKPaintStyle.Stroke,
             StrokeWidth = 2,
             Color = SKColors.Black
         };
 
-        SKPaint textPaint = new SKPaint
+        private SKPaint textPaint = new SKPaint
         {
             TextSize = 30.0f,
             Color = SKColors.Black,
@@ -55,6 +57,53 @@ namespace SkiaSharpSimpleCharts
             // Step 3 - Decorate bar chart
             canvas.Clear(SKColors.Orange);
             DrawDecoratedBars(canvas, args.Info);
+
+            // Step 4 - Animate bar chart
+            canvas.Clear(SKColors.Orange);
+            AnimateBars(canvas, args.Info);
+        }
+
+        private void AnimateBars(SKCanvas canvas, SKImageInfo info)
+        {
+            // Applying decorations 
+            var margin = 40;
+            var textHeight = 60;
+
+            var data = viewModel.GetDummyData();
+            var chart = new Chart() { Entries = data, ChartColor = SKColors.Blue };
+
+            var heighestBarValue = chart.Entries.Max(bar => bar.Value);
+
+            // Remove the left margin
+            var canvasWidth = info.Width - margin;
+            var canvasHeight = info.Height - margin * 2;
+            var barContentHeight = canvasHeight - textHeight;
+
+            // Takes spacing into account for bar width
+            var barWidth = (canvasWidth / chart.Entries.Count) - margin;
+
+            for (int index = 0; index < chart.Entries.Count; index++)
+            {
+                var currentBar = chart.Entries[index];
+                var procentageHeight = (float)currentBar.Value / heighestBarValue;
+
+                // Takes margin into height account
+                var barHeight = (int)(procentageHeight * barContentHeight) * easingAnimationProgress;
+
+                // Find start X
+                var startX = margin + (margin * index) + index * barWidth;
+                var startY = info.Height - margin - textHeight;
+
+                canvas.DrawRect(SKRect.Create(startX, startY, barWidth, -barHeight), barPaint);
+                canvas.DrawRect(SKRect.Create(startX, startY, barWidth, -barHeight), strokePaint);
+
+                var startTextX = startX + (barWidth / 2);
+                var startTextY = startY + textHeight;
+
+                textPaint.Color = SKColors.Black.WithAlpha((byte)(byte.MaxValue * easingAnimationProgress));
+
+                canvas.DrawText(currentBar.Title, startTextX, startTextY, textPaint);
+            }
         }
 
         private void DrawDecoratedBars(SKCanvas canvas, SKImageInfo info)
@@ -119,8 +168,27 @@ namespace SkiaSharpSimpleCharts
 
         private void OnRefreshClicked(object sender, EventArgs args)
         {
-            // Redraw the chart
-            barChart.InvalidateSurface();
+            easingAnimationProgress = 0.0f;
+            animationProgress = 0.0f;
+
+            // Runs 60 times pr. second.
+            Device.StartTimer(TimeSpan.FromSeconds(1.0f / 60), () =>
+            {
+                animationProgress += 0.025f;
+                easingAnimationProgress = (float)Easing.CubicIn.Ease(animationProgress);
+
+                // Redraw the chart
+                barChart.InvalidateSurface();
+
+                if (animationProgress >= 1.0f)
+                {
+                    animationProgress = 1.0f;
+                    easingAnimationProgress = 1.0f;
+                    return false;
+                }
+
+                return true;
+            });
         }
     }
 }
