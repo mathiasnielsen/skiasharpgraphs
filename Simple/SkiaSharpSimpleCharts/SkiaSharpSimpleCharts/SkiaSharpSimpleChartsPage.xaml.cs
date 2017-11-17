9da811db8ea3c68150dd0025cb1296c551318234
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-17 12:08:32 +0000

    simple canvas working

--- Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
+++ Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
 
 namespace SkiaSharpSimpleCharts
 {
@@ -7,6 +9,20 @@ namespace SkiaSharpSimpleCharts
         public SkiaSharpSimpleChartsPage()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when SKCanvas is drawn/invalidated
+        /// </summary>
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            // Step 1 - Simple background
+            var surface = args.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.Orange);
+
+            // Step 2 - Draw a coorda
         }
     }
 }
