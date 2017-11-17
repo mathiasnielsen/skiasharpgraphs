18465ef4fde306562221430288d1aa692fde5b63
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-17 14:40:33 +0000

    Margins

--- Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
+++ Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
@@ -11,6 +11,26 @@ namespace SkiaSharpSimpleCharts
     {
         private SkiaSharpSimpleChartsViewModel viewModel;
 
+        SKPaint barPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Blue
+        };
+
+        SKPaint strokePaint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            StrokeWidth = 2,
+            Color = SKColors.Black
+        };
+
+        SKPaint textPaint = new SKPaint
+        {
+            TextSize = 30.0f,
+            Color = SKColors.Black,
+            TextAlign = SKTextAlign.Center
+        };
+
         public SkiaSharpSimpleChartsPage()
         {
             InitializeComponent();
@@ -31,12 +51,50 @@ namespace SkiaSharpSimpleCharts
 
             // Step 2 - Draw a coordates
             DrawBars(canvas, args.Info);
+
+            // Step 3 - Decorate bar chart
+            canvas.Clear(SKColors.Orange);
+            DrawDecoratedBars(canvas, args.Info);
         }
 
-        private void OnRefreshClicked(object sender, EventArgs args)
+        private void DrawDecoratedBars(SKCanvas canvas, SKImageInfo info)
         {
-            // Redraw the chart
-            barChart.InvalidateSurface();
+            // Applying decorations 
+            var margin = 40;
+            var textHeight = 60;
+
+            var data = viewModel.GetDummyData();
+            var chart = new Chart() { Entries = data, ChartColor = SKColors.Blue };
+
+            var heighestBarValue = chart.Entries.Max(bar => bar.Value);
+
+            // Remove the left margin
+            var canvasWidth = info.Width - margin;
+            var canvasHeight = info.Height - margin * 2;
+            var barContentHeight = canvasHeight - textHeight;
+
+            // Takes spacing into account for bar width
+            var barWidth = (canvasWidth / chart.Entries.Count) - margin;
+
+            for (int index = 0; index < chart.Entries.Count; index++)
+            {
+                var currentBar = chart.Entries[index];
+                var procentageHeight = (float)currentBar.Value / heighestBarValue;
+
+                // Takes margin into height account
+                var barHeight = (int)(procentageHeight * barContentHeight);
+
+                // Find start X
+                var startX = margin + (margin * index) + index * barWidth;
+                var startY = info.Height - margin - textHeight;
+
+                canvas.DrawRect(SKRect.Create(startX, startY, barWidth, -barHeight), barPaint);
+                canvas.DrawRect(SKRect.Create(startX, startY, barWidth, -barHeight), strokePaint);
+
+                var startTextX = startX + (barWidth / 2);
+                var startTextY = startY + textHeight;
+                canvas.DrawText(currentBar.Title, startTextX, startTextY, textPaint);
+            }
         }
 
         private void DrawBars(SKCanvas canvas, SKImageInfo info)
@@ -50,27 +108,19 @@ namespace SkiaSharpSimpleCharts
             for (int index = 0; index < chart.Entries.Count; index++)
             {
                 var currentBar = chart.Entries[index];
+                var procentageHeight = (float)currentBar.Value / heighestBarValue;
+                var barHeight = (int)(procentageHeight * info.Height);
+                var startX = barWidth * index;
 
-                using (var barPaint = new SKPaint
-                {
-                    Style = SKPaintStyle.Fill,
-                    Color = chart.ChartColor
-                })
-                using (var strokePaint = new SKPaint
-                {
-                    Style = SKPaintStyle.Stroke,
-                    StrokeWidth = 2,
-                    Color = SKColors.Black
-                })
-                {
-                    var procentageHeight = (float)currentBar.Value / heighestBarValue;
-                    var barHeight = (int)(procentageHeight * info.Height);
-                    var startX = barWidth * index;
-
-                    canvas.DrawRect(SKRect.Create(startX, info.Height, barWidth, -barHeight), barPaint);
-                    canvas.DrawRect(SKRect.Create(startX, info.Height, barWidth, -barHeight), strokePaint);
-                }
+                canvas.DrawRect(SKRect.Create(startX, info.Height, barWidth, -barHeight), barPaint);
+                canvas.DrawRect(SKRect.Create(startX, info.Height, barWidth, -barHeight), strokePaint);
             }
+        }
+
+        private void OnRefreshClicked(object sender, EventArgs args)
+        {
+            // Redraw the chart
+            barChart.InvalidateSurface();
         }
     }
 }
