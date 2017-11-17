95324633110ad43cb4d81a2d9932cbe62be75a76
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-11-17 11:32:38 +0000

    Initial

---
+++ Simple/SkiaSharpSimpleCharts/SkiaSharpSimpleCharts/SkiaSharpSimpleChartsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SkiaSharpSimpleCharts
+{
+    public partial class SkiaSharpSimpleChartsPage : ContentPage
+    {
+        public SkiaSharpSimpleChartsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
